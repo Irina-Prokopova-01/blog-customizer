d8dd601118c7a96dc3dfb1583a96aec96f8c1447
Irina <Irina-prokopova.style@yandex.ru>
2026-09-07 13:38:55 -0700

    Реализована механика открытия-закрытия, настройки применяются, сбрасываются

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,19 +1,123 @@
 import { ArrowButton } from 'src/ui/arrow-button';
+import clsx from 'clsx';
 import { Button } from 'src/ui/button';
-// import { RadioGroup } from  'src/ui/radio-group';
-// import { Select } from 'src/ui/select';
-// import { Separator } from 'src/ui/separator';
-
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { useState, useEffect, useRef } from 'react';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
+import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	onApply: (params: ArticleStateType) => void;
+	onReset: () => void;
+};
+
+export const ArticleParamsForm = ({
+	onApply,
+	onReset,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const switchOpen = () => setIsOpen((isOpen) => !isOpen);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const sidebarRef = useRef<HTMLElement>(null);
+
+	// Универсальная функция для обновления полей формы
+	const updateFormField = <T,>(field: keyof ArticleStateType) => {
+		return (value: T) => {
+			setFormState((prev) => ({
+				...prev,
+				[field]: value,
+			}));
+		};
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onApply(formState);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onReset();
+	};
+
+	useEffect(() => {
+		if (!isOpen) return;
+
+		const handleClickOutside = (event: MouseEvent) => {
+			if (!sidebarRef.current?.contains(event.target as Node)) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => document.removeEventListener('mousedown', handleClickOutside);
+	}, [isOpen]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<div onClick={(e) => e.stopPropagation()}>
+				<ArrowButton isOpen={isOpen} onClick={switchOpen} />
+			</div>
+			<aside
+				ref={sidebarRef}
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={updateFormField('fontFamilyOption')}
+					/>
+					<RadioGroup
+						title='Размер шрифта'
+						name='fontSize'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={updateFormField('fontSizeOption')}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={updateFormField('fontColor')}
+					/>
+
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={updateFormField('backgroundColor')}
+					/>
+
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={updateFormField('contentWidth')}
+					/>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
